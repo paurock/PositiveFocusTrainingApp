77a22e517dc8e6dea0f354f659b1afbe69829156
Roman Tselenko <38553025+paurock@users.noreply.github.com>
2019-09-18 17:02:56 +0300

    Release v1.0.0
    New features added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import smile from "./images/smile.jpg";
+import sadSmile from "./images/sadSmile.jpg";
 
 import "./styles.css";
 
 function App() {
-  const [state, setState] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [pressedAlready, setPressedAlready] = useState(false);
+  const [rightAnswers, setRightAnswers] = useState(0);
+  const [currentScores, setCurrentScores] = useState(0);
+  const [currentLevel, setCurrentLevel] = useState(0);
+  const [interval, setNewInterval] = useState(1000);
+  const [message, setMessage] = useState("");
+  const [state, setState] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  ]);
+
   const getShuffledArr = arr => {
     const newArr = arr.slice();
     for (let i = newArr.length - 1; i > 0; i--) {
@@ -13,12 +43,75 @@ function App() {
     }
     setState(newArr);
   };
+  const levelUp = () => {
+    setCurrentLevel(currentLevel + 1);
+    setNewInterval(interval - 100);
+  };
+
+  const calculateCurrentLevel = () => {
+    if (rightAnswers >= 3) {
+      levelUp();
+      setRightAnswers(0);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => getShuffledArr(state), interval);
+    return () => {
+      setPressedAlready(false);
+      clearInterval(timer);
+    };
+  }, [interval, state]);
+
+  const checkItem = e => {
+    e.persist();
+    if (e.target.className === "smile" && pressedAlready === false) {
+      setMessage("Great!!! :)");
+      setPressedAlready(true);
+      setCurrentScores(Math.ceil(currentScores * (currentLevel + 1) + 10));
+      setRightAnswers(rightAnswers + 1);
+      calculateCurrentLevel();
+    } else if (pressedAlready === false) {
+      setMessage("Missed :(");
+      setPressedAlready(true);
+      setCurrentScores(
+        Math.ceil(currentScores - currentScores / (currentLevel + 1))
+      );
+      calculateCurrentLevel();
+    }
+  };
   return (
     <div className="App">
-      <button onClick={() => getShuffledArr(state)}>Randomize!</button>
-      {state.map(item => (
-        <p key={item}>{item}</p>
-      ))}
+      <div className="main-block">
+        <section className="info-block">
+          <p>
+            <span>Current Level:</span> {currentLevel}
+          </p>
+          <p>
+            <span>Money Earned:</span> {currentScores}$
+          </p>
+        </section>
+        <div
+          className={message === "Missed :(" ? "hit-title missed" : "hit-title"}
+        >
+          {message}
+        </div>
+        <section className="images">
+          <ul>
+            {state.map(item => (
+              <li className="image" key={item}>
+                <img
+                  onClick={e => checkItem(e)}
+                  key={item}
+                  className={item == 10 ? "smile" : "sadSmile"}
+                  src={item == 10 ? smile : sadSmile}
+                  alt="smile"
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
